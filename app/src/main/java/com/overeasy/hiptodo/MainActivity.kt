eda84fea8b2f75dbb45c0625b8cdbd8b9469ed03
OverEasy <33789695+lht1324@users.noreply.github.com>
2020-10-26 03:50:49 +0900

    Changed LinearLayout of recyclerView's item to ConstraintLayout

--- app/src/main/java/com/overeasy/hiptodo/MainActivity.kt
+++ app/src/main/java/com/overeasy/hiptodo/MainActivity.kt
@@ -21,17 +21,11 @@ class MainActivity : AppCompatActivity() {
 
     // 해야 할 것
     // Rx, MVVM 패턴 적용
-    // 캘린더뷰로 D-Day 설정
-    // 터치하면 이름이랑 날짜 설정하는 창 띄우기
-    // 팝업윈도우는 카드뷰 쓰면 되겠구만
-    // 삭제를 이미지 버튼으로 바꾸기
-    // 제목이 영역을 넘기면 ...으로 줄여주기
     // 할 일 순서 정해야 한다
-    // 캘린더 뷰(팝업 윈도우)
+    // something 텍스트뷰가 D-Day 텍스트뷰 영역을 침범한다
+    // something이 줄여지는 한이 있어도 D-Day는 그대로여야 한다
     // 정렬 (왼쪽을 터치하면 바 생성된 다음에 움직여서 정렬하는 걸로 할까?)
-    // 체크박스는 누르면 이펙트가 뜨던데 그냥 체크박스 쓸까?
     // D-Day가 정해졌으면 D-Day가 필요하지만 정해지지 않으면 굳이 신경쓸 필요가 없다
-    // 터치된 아이템의 Position에서 ToDo를 읽은 뒤 거기서 가져와야 한다
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
